cfbe1371a66beff5f1ac4d8c4b1b4f22b2bb28ae
ssr0016 <samsonrecluta0016@gmail.com>
2024-03-21 11:10:23 +0800

    add some style

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -16,6 +16,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	files := []string{
 		"cmd/ui/html/base.tmpl",
+		"cmd/ui/html/partials/nav.tmpl",
 		"cmd/ui/html/pages/home.tmpl",
 	}
 
@@ -26,7 +27,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
